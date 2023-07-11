16d83b6217e6dbe376f09e38c5f59cbcfeb2f7d3
khayatcode <a.khayatcode0103@gmail.com>
2023-07-11 14:16:44 -0600

    init

--- client/src/components/Reg.jsx
+++ client/src/components/Reg.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useNavigate, Link } from "react-router-dom";
 
 const Reg = (props) => {
