c60d6e781ba53effcda59daf95faf81342c541ae
khayatcode <a.khayatcode0103@gmail.com>
2023-07-13 00:37:17 -0600

    all catergories css

--- client/src/components/AllCategories.jsx
+++ client/src/components/AllCategories.jsx
@@ -9,64 +9,51 @@ import '../css/AllCategories.css'
 const AllCategories = () => {
     return (
 
-        <div className='container-fluid bg-info'>
+        <div className='container-fluid bg-inf pb-3'>
             <div className='row text-start mb-3 mt-3 p-5'>
                 <h2 style={{ fontWeight: 300 }}>Fabriko From Head-to-Toe</h2>
             </div>
-            <div className='row d-flex justify-content-around align-items-center pb-5 bg-danger' style={{ height: 'auto' }}>
-                <Link to={"category/upper"} className='text-decoration-none col-md-3 col-sm-6 bg-success d-flex flex-column align-items-center'>
-                    <div className="image-container">
+            <hr  className="d-md-none pb-2"/>
+            <div className='row d-flex justify-content-evenly align-items-center bg-danger '>
+                <Link to={"category/upper"} className='text-decoration-none col-md-3 col-sm-6  bg-success d-flex flex-column align-items-center'>
+                    <div className="image-container bg-warnin">
                         <img
-                            className='img-fluid p-3 image'
+                            className='img-fluid p-3 image '
                             src={UpperWear}
                             alt='UpperWear'
-                            onMouseOver={(e) => {
-                                e.currentTarget.style.transform = 'scale(1.05)';
-                            }}
-                            onMouseOut={(e) => {
-                                e.currentTarget.style.transform = 'scale(1)';
-                            }}
+                            style={{backgroundColor: '#E8E8E8' }}
                         />
                     </div>
-                    <h5 className='mt-3' style={{ textAlign: 'center', fontWeight: 300, color: 'black' }}>Upper Wear</h5>
+                        <h5 className='mt-3' style={{ fontWeight: 300, color: 'black' }}>Upper Wear</h5>
                 </Link>
+                <hr className="d-md-none" />
+                <br />
                 <Link to={"category/bottom"} className='text-decoration-none col-md-3 col-sm-6 bg-success d-flex flex-column align-items-center'>
-                    <div className="image-container">
+                    <div className="image-container bg-warnin ">
                         <img
-                            className='pb-3 img-fluid image'
+                            className='img-fluid image'
                             src={BottomWear}
                             alt='BottomWear'
-                            onMouseOver={(e) => {
-                                e.currentTarget.style.transform = 'scale(1.05)';
-                            }}
-                            onMouseOut={(e) => {
-                                e.currentTarget.style.transform = 'scale(1)';
-                            }}
+                            style={{backgroundColor: '#E8E8E8',  objectPosition: 'top'}}
                         />
                     </div>
                     <h5 className='mt-3' style={{ textAlign: 'center', fontWeight: 300, color: 'black' }}>Bottom Wear</h5>
                 </Link>
-                <Link to={"category/shoes"} className='text-decoration-none col-md-3 col-sm-6 bg-success d-flex flex-column align-items-center'>
-                    <div className="image-container">
+                <hr className="d-md-none" />
+                <br />
+                <Link to={"category/shoes"} className='text-decoration-none col-md-3 col-sm-12 bg-success d-flex flex-column align-items-center'>
+                    <div className="image-container bg-warnin">
                         <img
                             className='img-fluid image'
                             src={DressShoes}
                             alt='DressShoes'
-                            onMouseOver={(e) => {
-                                e.currentTarget.style.transform = 'scale(1.05)';
-                            }}
-                            onMouseOut={(e) => {
-                                e.currentTarget.style.transform = 'scale(1)';
-                            }}
                         />
                     </div>
                     <h5 className='mt-3' style={{ textAlign: 'center', fontWeight: 300, color: 'black' }}>Dress Shoes</h5>
                 </Link>
+                <br />
             </div>
         </div>
-
-
-
 
     )
 }
