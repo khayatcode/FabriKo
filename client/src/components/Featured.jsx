bd3dd85f2fccd8cb27267c82ddd501ada370972c
khayatcode <a.khayatcode0103@gmail.com>
2023-07-13 21:34:50 -0600

    fixed allFeature comp css

--- client/src/components/Featured.jsx
+++ client/src/components/Featured.jsx
@@ -4,17 +4,18 @@ import featured2 from '../images/featured2.jpeg'
 import featured3 from '../images/featured3.jpeg'
 import featured4 from '../images/featured4.jpeg'
 import featured5 from '../images/featured5.jpeg'
+import '../css/Featured.css'
 
 const Featured = () => {
   return (
     <div className='p-5'>
         <h3>Featured in</h3>
-        <div className='row d-flex justify-content-evenly align-items-center p-5'>
-            <img src={featured1} alt='featured1' style={{ width: '20%' }} />
-            <img src={featured2} alt='featured2' style={{ width: '8%' }} />
-            <img src={featured3} alt='featured3' style={{ width: '12%' }} />
-            <img src={featured4} alt='featured4' style={{ width: '11%' }} />
-            <img src={featured5} alt='featured5' style={{ width: '18%' }} />
+        <div className='row d-flex justify-content-evenly align-items-center p-5 gap-5 imageDiv'>
+            <img src={featured1} alt='featured1' className='image-feat'/>
+            <img src={featured2} alt='featured2' className='image-feat'/>
+            <img src={featured3} alt='featured3' className='image-feat'/>
+            <img src={featured4} alt='featured4' className='image-feat'/>
+            <img src={featured5} alt='featured5' className='image-feat'/>
         </div>
     </div>
   )
